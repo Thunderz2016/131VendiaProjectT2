2a5f34279d50e2ce4bb9c510127fd1e71370722c
Maxwell1 <90221081+MaxwellAbah1@users.noreply.github.com>
2023-09-15 18:13:23 -0700

    second commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Configuration options here
+  build: {
+    // Build options here
+  },
+  // Other configuration options
+});

aae13b9d199b56a36640fc7192741b1fe94eab59
Maxwell1 <90221081+MaxwellAbah1@users.noreply.github.com>
2023-10-15 13:05:51 -0700

    edit nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,19 @@
 import {
   Box, Button,
-  Flex, VStack,
-  Input, Text,
-  Menu, MenuButton,
-  MenuList, MenuItem, HStack } from "@chakra-ui/react";
+   Text, HStack, VStack } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { Link } from "react-router-dom";
 import { 
-  HamburgerIcon, EditIcon, 
-  DeleteIcon, AddIcon, StarIcon, ViewIcon  } from "@chakra-ui/icons";
+  EditIcon, DeleteIcon, AddIcon, ViewIcon  } from "@chakra-ui/icons";
 import { auth } from "../firebase";
-//import { Navigate } from "react-router-dom";
-import { IconButton } from "@chakra-ui/react";
 import {
   Drawer,
   DrawerBody,
   DrawerFooter,
-  DrawerHeader,
   DrawerOverlay,
   DrawerContent,
-  DrawerCloseButton, RadioGroup, Stack, Radio
+  DrawerCloseButton,
 } from '@chakra-ui/react'
 import { useDisclosure } from "@chakra-ui/react";
 import React from "react";
@@ -38,14 +31,13 @@ const Navbar = () => {
   }
 
   const [authUser, setAuthUser] = useState(null);
-  const [placement, setPlacement ] = useState();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = React.useRef()
+  const btnRef = React.useRef() // from Chakra UI
   const sizes = ['xs'] // size of the Drawer
-  const [size, setSize] = React.useState('')
+  const [size, setSize] = React.useState('') // from Chakra UI
 
 
-  const userSignOut = () => {
+const userSignOut = () => { {/* Signout fronm firebase*/}
     signOut(auth)
       .then(() => {
         console.log("sign out successful");
@@ -55,109 +47,99 @@ const Navbar = () => {
   };
 
 
-
   return (
     <Box bg="orange" py={3} px={3} boxShadow="md" position="sticky" top="0">
 
-     <> {/* Button on the Page, can also control size*/}
-     {sizes.map((size) => (
-      <Button ref={btnRef} 
-              colorScheme='teal' 
-              onClick={() => handleClick(size)}
-              key={size}
-              m={4}>
-        {`Menu`}
-      </Button>
-      ))}
+      <> {/* The Physical Button that opens the drawer */}
+        {sizes.map((size) => (
+          
+          <Button 
+            ref={btnRef}
+            colorScheme='teal'
+            onClick={() => handleClick(size)}
+            key={size}
+            m={4}
+          >
+            {`Menu`}
+          </Button>
+        ))}
 
+        {/* The direction the drawer opens*/}
 
-      <Drawer //The drawer opening
-        isOpen={isOpen}
-        placement='left'
-        onClose={onClose}
-        finalFocusRef={btnRef}
-        size={size}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerCloseButton />
+        <Drawer
+          isOpen={isOpen}
+          placement='left' // can be changed
+          onClose={onClose}
+          finalFocusRef={btnRef}
+          size={size}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <DrawerCloseButton /> {/*Top right close button*/}
 
-   <DrawerBody> {/* This is what is contained in the body of the drawer*/}
+            <DrawerBody> {/* This stores everything in the drawer*/}
 
-    <Menu>
-            <MenuButton
-            as={IconButton}
-            aria-label='Options'
-            icon={<HamburgerIcon />}
-            variant='outline'/>
-
-        <MenuList>
-
-            <Link to="/Device">
-              <MenuItem icon={<AddIcon />} >
-                Device Add
-              </MenuItem>
-            </Link>
-
-            <Link to="/DeviceUpdate">
-              <MenuItem icon={<EditIcon />} >
-                Device Update
-              </MenuItem>
-            </Link>
-
-            <Link to="/DeleteDevice">
-              <MenuItem icon={<DeleteIcon />} >
-                Delete
-              </MenuItem>
-            </Link>
-
-            <Link to="/ListDevice">
-              <MenuItem icon={<ViewIcon />} >
-                List Page
-              </MenuItem>
-            </Link>
-
-            <Link to="/ListID">
-              <MenuItem icon={<ViewIcon />} >
-                List ID
-              </MenuItem>
-            </Link>
-        
-        </MenuList>
-        
-    </Menu>
-            
-           
-  </DrawerBody>
-
-          {/* Button of Page*/}
-
-          <DrawerFooter>
-            <HStack spacing='24px'>
-              <Link to="/">
-                <Button colorScheme="blue" type="submit">
-                  Log In
-                 </Button>
+              <VStack spacing="4"> {/* This control the list spacing of the other pages*/}
+              <Link to="/Device">
+                <Box p={2} display="flex" alignItems="center">
+                  <AddIcon mr={2} />
+                  Device Add
+                </Box>
               </Link>
-              
-              {authUser ? (
-              <>
-              {/*<Text textAlign="center">{`Signed In as ${authUser.email}`}</Text>*/}
-              <Button  marginLeft="auto" colorScheme="red" onClick={userSignOut}>
-                Sign Out
-              </Button>
-              </>
-              ) : (
-              <Text textAlign="center">Signed Out</Text>
-              )}
-            </HStack>
 
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    </>
+              <Link to="/DeviceUpdate">
+                <Box p={2} display="flex" alignItems="center">
+                  <EditIcon mr={2} />
+                  Device Update
+                </Box>
+              </Link>
 
-    
+              <Link to="/DeleteDevice">
+                <Box p={2} display="flex" alignItems="center">
+                  <DeleteIcon mr={2} />
+                  Delete
+                </Box>
+              </Link>
+
+              <Link to="/ListDevice">
+                <Box p={2} display="flex" alignItems="center">
+                  <ViewIcon mr={2} />
+                  List Page
+                </Box>
+              </Link>
+
+              <Link to="/ListID">
+                <Box p={2} display="flex" alignItems="center">
+                  <ViewIcon mr={2} />
+                  List ID
+                </Box>
+              </Link>
+              </VStack>
+            </DrawerBody> {/* This ENDS stores everything in the drawer*/}
+
+            <DrawerFooter> {/* This stores everything in the Bottom drawer*/}
+              <HStack spacing='30px'> {/*Spacing between the two buttons*/}
+                <Link to="/">
+                  <Button colorScheme="blue" type="submit">
+                    Log In
+                  </Button>
+                </Link>
+
+
+                {authUser ? (
+                  <>
+                    <Button marginLeft="auto" colorScheme="red" onClick={userSignOut}>
+                      Sign Out
+                    </Button>
+                  </>
+                ) : (
+                  <Text textAlign="center">Signed Out</Text>
+                )}
+              </HStack>
+            </DrawerFooter> {/* This ENDs stores everything in the Bottom drawer*/}
+          </DrawerContent> {/* This ENDs ALL contents in the drawer*/}
+        </Drawer>
+      </>
     </Box>
   );
 };

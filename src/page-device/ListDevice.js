7a7cdccad45aa1d4142126297c6027d53e478fad
Thunderz2016 <24950670+Thunderz2016@users.noreply.github.com>
2023-10-24 15:43:04 -0700

    Implement editable to all fields in listDevice

--- src/page-device/ListDevice.js
+++ src/page-device/ListDevice.js
@@ -1,12 +1,7 @@
-
 import React, { useEffect, useState } from "react";
 import { vendiaClient } from "../vendiaClient";
 import { Stack, Box, Text, Select } from "@chakra-ui/react";
-import {
-  Table, Thead,
-  Tbody,Tr, Th, Button,
-  Td
-} from '@chakra-ui/react'
+import { Table, Thead, Tbody, Tr, Th, Button, Td } from "@chakra-ui/react";
 import { useDisclosure } from "@chakra-ui/react";
 import {
   Modal,
@@ -16,15 +11,20 @@ import {
   ModalFooter,
   ModalBody,
   ModalCloseButton,
-} from '@chakra-ui/react'
+} from "@chakra-ui/react";
 import { Input } from "@chakra-ui/react"; // Using Chakra UI components
-
+import {
+  Editable,
+  EditableInput,
+  EditableTextarea,
+  EditablePreview,
+} from "@chakra-ui/react";
+import { Switch } from "@chakra-ui/react";
 
 const { client } = vendiaClient();
 
 export const List = () => {
-
- // const [deviceId, setdeviceId] = useState("");
+  // const [deviceId, setdeviceId] = useState("");
   const [deviceId, setDeviceId] = useState(""); // State for Device ID
   // Change state variable name to "devices" for storing the list of devices
   const [devices, setDevices] = useState("");
@@ -37,9 +37,8 @@ export const List = () => {
   const [completed, setCompleted] = useState("");
   const [updatedBy, setUpdatedBy] = useState("");
   const [testList, setTestList] = useState([]);
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const [searchText, setSearchText] = useState("");
-
 
   //const navigate = useNavigate();
 
@@ -47,44 +46,44 @@ export const List = () => {
     const listTest = async () => {
       const listTestResponse = await client.entities.test.list();
       console.log(listTestResponse?.items);
-      
+
       // If searchText is empty, display all items. Otherwise, filter based on the searchText.
       const filteredTests = searchText
-        ? listTestResponse?.items.filter(test =>
+        ? listTestResponse?.items.filter((test) =>
             Object.values(test).some(
-              val => typeof val === "string" && val.toLowerCase().includes(searchText.toLowerCase())
+              (val) =>
+                typeof val === "string" &&
+                val.toLowerCase().includes(searchText.toLowerCase())
             )
           )
         : listTestResponse?.items;
-  
+
       setTestList(filteredTests);
     };
-  
+
     listTest();
-  }, [searchText]);  // Added searchText as a dependency
-  
-  
+  }, [searchText]); // Added searchText as a dependency
+
   useEffect(() => {
     if (deviceId) {
+      // Fetch the device information based on the deviceId
+      const fetchDevice = async () => {
+        const deviceResponse = await client.entities.test.get(deviceId);
 
-    // Fetch the device information based on the deviceId
-    const fetchDevice = async () => {
-      const deviceResponse = await client.entities.test.get(deviceId);
+        // Setting the retrieved data into respective state variables
+        setDevice(deviceResponse.Device);
+        setTestID(deviceResponse.TestID.toString());
+        setOrgAssignment(deviceResponse.OrgAssignment);
+        setTestName(deviceResponse.TestName);
+        setTestMethod(deviceResponse.TestMethod);
+        setNotes(deviceResponse.Notes);
+        setCompleted(deviceResponse.Completed);
+        setUpdatedBy(deviceResponse.UpdatedBy);
+      };
 
-      // Setting the retrieved data into respective state variables
-      setDevice(deviceResponse.Device);
-      setTestID(deviceResponse.TestID.toString());
-      setOrgAssignment(deviceResponse.OrgAssignment);
-      setTestName(deviceResponse.TestName);
-      setTestMethod(deviceResponse.TestMethod);
-      setNotes(deviceResponse.Notes);
-      setCompleted(deviceResponse.Completed);
-      setUpdatedBy(deviceResponse.UpdatedBy);
-    };
-
-    fetchDevice(); // Calling the fetchDevice function when 'deviceId' changes
-  }
-  }, [deviceId]); 
+      fetchDevice(); // Calling the fetchDevice function when 'deviceId' changes
+    }
+  }, [deviceId]);
 
   const listDevice = async () => {
     // Add a new prod uct
@@ -101,43 +100,42 @@ export const List = () => {
     });
     console.log(listDeviceResponse);
   };
-  
+
   const updateDevice = async () => {
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Preventing the default form submission behavior
-    updateDevice(); // Calling the updateDevice function to update the device
+    const handleSubmit = (event) => {
+      event.preventDefault(); // Preventing the default form submission behavior
+      updateDevice(); // Calling the updateDevice function to update the device
+    };
   };
-};
 
   return (
     <Stack spacing={4}>
-      <Text fontSize="xl" align="center">List Device Page</Text>
+      <Text fontSize="xl" align="center">
+        <p>List Device Page</p>
+        <p>Ciick on a field to edit</p>
+      </Text>
 
       <Stack>
-
-          <Input
-            placeholder="Search for a test..."
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            size="md"
-            width="500px"
-            textAlign="center"
-             mb={4}  // margin-bottom for some spacing
-             />
-
+        <Input
+          placeholder="Search for a test..."
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+          size="md"
+          width="500px"
+          textAlign="center"
+          mb={4} // margin-bottom for some spacing
+        />
       </Stack>
-  
+
       <Box overflowX="auto" align="center">
-
         <Table>
-
           <Thead>
             <Tr>
               <Th>Device</Th>
               <Th>TestID</Th>
               <Th>OrgAssignment</Th>
               <Th>TestName</Th>
-              <Th fontWeight='extrabold'>TestMethod</Th>
+              <Th fontWeight="extrabold">TestMethod</Th>
               <Th>Notes</Th>
               <Th>Completed</Th>
               <Th>UpdatedBy</Th>
@@ -146,144 +144,184 @@ export const List = () => {
 
           <Tbody>
             {testList.map((test, index) => (
+              // EditablePreview needs width="full" to account for empty default values,
+              // otherwise the editable isn't clickable.
               <Tr key={index}>
-                <Td>{test.Device}</Td>
-                <Td>{test.TestID}</Td>
-                <Td>{test.OrgAssignment}</Td>
-                <Td>{test.TestName}</Td>
-                <Td>{test.TestMethod}</Td>
-                <Td>{test.Notes}</Td>
-                <Td>{test.Completed.toString()}</Td>
-                <Td>{test.UpdatedBy}</Td>
+                <Td>
+                  <Editable defaultValue={test.Device}>
+                    <EditablePreview width="full" />
+                    <EditableTextarea />
+                  </Editable>
+                </Td>
+                <Td>
+                  {" "}
+                  <Editable defaultValue={test.TestID}>
+                    <EditablePreview width="full" />
+                    <EditableTextarea />
+                  </Editable>
+                </Td>
+                <Td>
+                  <Editable defaultValue={test.OrgAssignment}>
+                    <EditablePreview width="full" />
+                    <EditableTextarea />
+                  </Editable>
+                </Td>
+                <Td>
+                  <Editable defaultValue={test.TestName}>
+                    <EditablePreview width="full" />
+                    <EditableTextarea />
+                  </Editable>
+                </Td>
+                <Td>
+                  <Editable defaultValue={test.TestMethod}>
+                    <EditablePreview width="full" />
+                    <EditableTextarea />
+                  </Editable>
+                </Td>
+                <Td>
+                  <Editable defaultValue={test.Notes}>
+                    <EditablePreview width="full" />
+                    <EditableTextarea />
+                  </Editable>
+                </Td>
+                <Td>
+                  {<Switch id={test.Completed} />}
+                </Td>
+                <Td>
+                  <Editable defaultValue={test.UpdatedBy}>
+                    <EditablePreview width="full"/>
+                    <EditableTextarea />
+                  </Editable>
+                </Td>
+                <Td>
+                  <Button colorScheme="blue">Save Changes</Button>
+                </Td>
               </Tr>
             ))}
           </Tbody>
-
         </Table>
-
       </Box>
 
       {/*All This Shit is Just the pop-up Edit Button*/}
-      <Stack align="center"> 
-      <>
-        <Button onClick={onOpen}>EDIT</Button>
+      <Stack align="center">
+        <>
+          <Button onClick={onOpen}>EDIT</Button>
           <Modal isOpen={isOpen} onClose={onClose}>
             <ModalOverlay />
             <ModalContent>
               <ModalHeader align="center">Edit Device</ModalHeader>
               <ModalCloseButton />
               <ModalBody>
-              <Stack spacing={4} direction="column" align="center" justify="center">
+                <Stack
+                  spacing={4}
+                  direction="column"
+                  align="center"
+                  justify="center"
+                >
+                  <Select
+                    placeholder="Select option"
+                    value={deviceId}
+                    onChange={(e) => setDeviceId(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  >
+                    {testList.map((test, index) => (
+                      <option key={test._id} value={test._id}>
+                        {test.Device} - {test.OrgAssignment}
+                      </option>
+                    ))}
+                  </Select>
 
-<Select
-  placeholder="Select option"
-  value={deviceId}
-  onChange={(e) => setDeviceId(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center">
-  {testList.map((test, index) => (
-    <option key={test._id} value={test._id}>
-      {test.Device} - {test.OrgAssignment}
-    </option>
-  ))}
-</Select>
+                  <Input
+                    placeholder="Device"
+                    value={device}
+                    onChange={(e) => setDevice(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="Device"
-  value={device}
-  onChange={(e) => setDevice(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Input
+                    placeholder="TestID[Integer]"
+                    value={testID}
+                    onChange={(e) => setTestID(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="TestID[Integer]"
-  value={testID}
-  onChange={(e) => setTestID(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Input
+                    placeholder="OrgAssignment"
+                    value={orgAssignment}
+                    onChange={(e) => setOrgAssignment(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="OrgAssignment"
-  value={orgAssignment}
-  onChange={(e) => setOrgAssignment(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Input
+                    placeholder="TestName"
+                    value={testName}
+                    onChange={(e) => setTestName(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="TestName"
-  value={testName}
-  onChange={(e) => setTestName(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Input
+                    placeholder="TestMethod"
+                    value={testMethod}
+                    onChange={(e) => setTestMethod(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="TestMethod"
-  value={testMethod}
-  onChange={(e) => setTestMethod(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Input
+                    placeholder="Notes"
+                    value={notes}
+                    onChange={(e) => setNotes(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="Notes"
-  value={notes}
-  onChange={(e) => setNotes(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Input
+                    placeholder="Completed[Boolean]"
+                    value={completed}
+                    onChange={(e) => setCompleted(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="Completed[Boolean]"
-  value={completed}
-  onChange={(e) => setCompleted(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Input
+                    placeholder="UpdatedBy"
+                    value={updatedBy}
+                    onChange={(e) => setUpdatedBy(e.target.value)}
+                    size="md"
+                    width="250px"
+                    textAlign="center"
+                  />
 
-<Input
-  placeholder="UpdatedBy"
-  value={updatedBy}
-  onChange={(e) => setUpdatedBy(e.target.value)}
-  size="md"
-  width="250px"
-  textAlign="center"
-/>
+                  <Button colorScheme="blue" type="submit">
+                    Update Device
+                  </Button>
 
-<Button colorScheme="blue" type="submit">
-  Update Device
-</Button>
-
-<Button colorScheme='blue' mr={3} onClick={onClose}>
-  Close
-</Button>
-
-</Stack>
-               
+                  <Button colorScheme="blue" mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                </Stack>
               </ModalBody>
-    
-              <ModalFooter>
 
-              </ModalFooter>
+              <ModalFooter></ModalFooter>
             </ModalContent>
           </Modal>
-          </>
-
+        </>
       </Stack>
     </Stack>
   );
-  
 };
 
 export default List;
